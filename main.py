e76e823f104f1d64287d8d26cff5806f831addd9
eunseoles <leeeunseo061206@gmail.com>
2025-12-09 14:07:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-text file a
+first = input("Enter the first number: ")
+second = input("Enter the second number: ")
+
+num1 = int(first)
+num2 = int(second)
+
+sum_result = num1 + num2
+mul_result = num1 * num2
+
+print("Sum: ", sum_result)
+print("Product: ", mul_result)
